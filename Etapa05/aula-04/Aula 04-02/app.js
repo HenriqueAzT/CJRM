fc2a6b5f9a7c53baf128f01247856dbedade4520
Henrique de Azara Tosta <henriqueazara3311@gmail.com>
2023-02-06 18:28:20 -0300

    Day 11

--- Etapa05/aula-04/Aula 04-02/app.js
+++ Etapa05/aula-04/Aula 04-02/app.js
@@ -2,21 +2,18 @@ const ul = document.querySelector('ul')
 
 // ul.remove()
 
-
-const lis = document.querySelectorAll('li')
-
-lis.forEach(li => {
-    li.addEventListener('click', (event) => {
-        const clickedElement = event.target
-
-        clickedElement.remove()
-    })
-})
-
 const button = document.querySelector('button')
 
 button.addEventListener('click', () => {
     const li = document.createElement('li')
     li.textContent = 'Novo Item'
     ul.append(li) // O append é utilizado para inserir um elemento no final, já o prepend no começo
+})
+
+ul.addEventListener('click', (event) => {
+    const clickedElement = event.target
+
+    if (clickedElement.tagName === 'LI') {
+        clickedElement.remove()
+    }
 })
