091a0a3c8d2b0a321989cfcc178df42d0678fa2c
Henrique de Azara Tosta <henriqueazara3311@gmail.com>
2023-02-08 15:34:10 -0300

    Day 13

--- Etapa06/aula-02/exercicio-18/app.js
+++ Etapa06/aula-02/exercicio-18/app.js
@@ -20,26 +20,93 @@
   
   Dica: pesquise pelo método "insertAdjacentElement", no MDN;
 */
-
+const inputUsername = document.querySelector('#username')
+const paragraphUsernameFeedback = document.createElement('p')
+const paragraphSubmitFeedback = document.createElement('p')
+paragraphUsernameFeedback.setAttribute('data-feedback', 'username-feedback')
+const button = document.querySelector('button')
 const form = document.querySelector('form')
 
-const regex = /[a-zA-Z]{6,}/
+paragraphSubmitFeedback.setAttribute('data-feedback', 'submit-feedback')
 
-form.addEventListener('keyup', (event, element) => {
-  const regexIsValid = regex.test(event.target.value)
+const invalidSubmitInfo = {
+  paragraph: paragraphSubmitFeedback,
+  text: 'Por favor, insira um username válido',
+  className: 'submit-help-feedback',
+  previousSibling: button
+}
+const validSubmitInfo = {
+  paragraph: paragraphSubmitFeedback,
+  text: 'Dados enviados =)',
+  className: 'submit-success-feedback',
+  previousSibling: button
+}
 
-  if (regexIsValid) {
-    form.insertAdjacentHTML('afterend', 'Username válido =)').setAttribute('class', 'username-success-feedback');
-  }else{
-    form.username.setAttribute('class', 'error')
+const invalidUsernameInfo = {
+  paragraph: paragraphUsernameFeedback,
+  text: 'O valor deve conter no mínimo 6 caracteres, com apenas letras maiúsculas e/ou minúsculas',
+  className: 'username-help-feedback',
+  previousSibling: inputUsername
+}
+
+const validUsernameInfo = {
+  paragraph: paragraphUsernameFeedback,
+  text: 'Username válido =)',
+  className: 'username-success-feedback',
+  previousSibling: inputUsername
+}
+
+const insertParagraphIntoDOM = paragraphInfo => {
+  const {paragraph, text, className, previousSibling} = paragraphInfo
+  paragraph.textContent = text
+  paragraph.setAttribute('class', className)
+  previousSibling.insertAdjacentElement('afterend', paragraph)
+}
+
+const removeSubmitParagraph = () => {
+  const paragraphSubmitFeedbackExists = document
+  .querySelector('[data-feedback="submit-feedback"]')
+
+  if (paragraphSubmitFeedbackExists) {
+    paragraphSubmitFeedback.remove()
   }
-})
+}
+
+const testUsername = inputValue => /^[a-zA-Z]{6,}$/.test(inputValue)
+
+const showUsernameInfo = event => {
+  const isUsernameValid = testUsername(event.target.value)
+
+  removeSubmitParagraph()
+
+  if (!isUsernameValid) {
+    insertParagraphIntoDOM(invalidUsernameInfo)
+  }else{
+    insertParagraphIntoDOM(validUsernameInfo)
+  }
+}
+
+
+const showSubmitInfo = event => {
+  event.preventDefault()  
+  const isUsernameValid = testUsername(inputUsername.value)
+  
+  if (!isUsernameValid) {
+    insertParagraphIntoDOM(invalidSubmitInfo)
+  }else{
+    insertParagraphIntoDOM(validSubmitInfo)
+  }
+}
+
+inputUsername.addEventListener('input', showUsernameInfo)
+form.addEventListener('submit', showSubmitInfo)
 
 /*
+
   02
 
-  - Valide o envio do form;
-  - Se o username inserido no input é válido, no envio do form, exiba um  
+  - Valide o envio do inputUsername;
+  - Se o username inserido no input é válido, no envio do inputUsername, exiba um  
     parágrafo verde abaixo do botão com a mensagem "Dados enviados =)";
   - Se no momento do envio, o valor do input é inválido, o parágrafo deve ser  
     vermelho e exibir "Por favor, insira um username válido".
@@ -64,3 +131,17 @@ form.addEventListener('keyup', (event, element) => {
         6;
     2) Pesquisar no MDN.
 */
+
+const some = (array, func) => {
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+    if (func(array[i])) {
+      return true
+    }
+  }
+
+  return false
+}
+
+console.log(some([1, 2, 3], item => item === 2));
+console.log(some([4, 5, 6], item => item === 3));
