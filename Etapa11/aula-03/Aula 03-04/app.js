a88aaae535fa35250ea7470652c410aa680fde78
Henrique de Azara Tosta <henriqueazara3311@gmail.com>
2023-02-23 11:13:54 -0300

    Day 22

--- Etapa11/aula-03/Aula 03-04/app.js
+++ Etapa11/aula-03/Aula 03-04/app.js
@@ -1,10 +1,30 @@
-const request = new XMLHttpRequest()
+const getTodos = (url, callback) => {
+    const request = new XMLHttpRequest()
 
-request.addEventListener('readystatechange', () => {
-    if(request.readyState === 4 && request.status === 200){
-        console.log(request.responseText);
+    request.addEventListener('readystatechange', () => {
+    const isRequestOK = request.readyState === 4 && request.status === 200
+    const isRequestNotOK = request.readyState === 4
+
+    if (isRequestOK) {
+        const data = JSON.parse(request.responseText)
+        callback(null, data)
+        return
     }
-})
+    if(isRequestNotOK){
+        callback('Não foi possível obter os dados', null)
+    }
+    })
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos')
-request.send()
+    request.open('GET', url)
+    request.send()
+}
+
+getTodos('./json/todos.json', (data) => {
+    console.log(data);
+    getTodos('./json/todos-02.json', (data) => {
+        console.log(data);
+        getTodos('./json/todos-03.json', (data) => {
+            console.log(data);
+        })
+    })
+})
