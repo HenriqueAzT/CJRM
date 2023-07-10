045f375d031030a1956b0a9673ce7f96416fba54
Henrique de Azara Tosta <henriqueazara3311@gmail.com>
2023-07-10 11:11:23 -0300

    Day 39

---
+++ Etapa15/aula-01/Aula 01-06/app.js
@@ -0,0 +1,11 @@
+const numbers = [7, 3, 4, 5, 3.5, 9]
+
+console.log(Math.min(...numbers))
+console.log(Math.max(...numbers))
+
+
+// const getReversedString = string => [...string]
+//     .reverse()
+//     .join('')
+
+// console.log(getReversedString('123'));
